8aca9380e25eb1d7fb70d040ad872236b425ba87
Shad <shad12@outlook.com>
2022-01-17 13:08:14 -0800

    linked

--- express-backend/index.js
+++ express-backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 
 const users = {
     users_list:
